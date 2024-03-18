21e55e93395761c0fac215a1750dad4b8953fa34
GsLKS <84880155+GsLKS@users.noreply.github.com>
2024-03-17 21:07:27 -0300

    Add files via upload

--- libs/at-err/index.js
+++ libs/at-err/index.js
@@ -2,6 +2,9 @@
 @ MyInfo:AT-ERR - Built in Original 'Anti-Error 0.0.2'
 @ Otimized and native version for 'MyInfo'
 */
+
+// at-err 0.2 (Released in the v1.1.0)
+
 let sE = false;
 
 function use(value) {
@@ -9,22 +12,15 @@ function use(value) {
   if (sE) {
     process.on('uncaughtException', function (err) {
       // console.error('...', err.message);
-      console.log("[Broken Alert]: Your 'MyInfo' is broken, try to fix with the 'mifix' command.")
+      console.log("\x1b[3m[Broken Alert]: Your 'MyInfo' binaries it's broken, try to fix using the 'mifix' command.\x1b[0m")
       return;
     });
   }
 }
 
-function log(message) {
-  if (sE) {
-    console.log(message);
-  } else {
-    // console.log('[MyInfo:AT-ERR] is not enabled');
-  }
-}
+// if false => console.log('[MyInfo:AT-ERR] is not enabled');
 
 module.exports = {
-  use,
-  log,
-  stoppedLog: 'An error was avoided',
+  use
+  stoppedLog: 'Error Detected',
 };
